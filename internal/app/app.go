a2ba715e8f8c7c0b815342942fc916a6c35f54c2
Viktor <froggod217@gmail.com>
2025-12-28 20:06:41 +0300

    fix: deleted documented code

--- internal/app/app.go
+++ internal/app/app.go
@@ -22,10 +22,6 @@ func Run() error {
 	}
 	defer db.Close()
 
-	// dbCert := database.NewDBCertificateTLS(db)
-	// serviceCert := services.NewCertificateTLSService(dbCert)
-	// handlerCert := transport.NewHandler(serviceCert)
-
 	dbCert := database.NewDataBase(db)
 	serviceCert := services.NewService(dbCert)
 	handlerCert := transport.NewHandler(serviceCert)
